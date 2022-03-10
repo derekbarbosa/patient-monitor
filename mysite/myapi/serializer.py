15d28148c345b8c52533d2259eae6e4486629d08
Derek Barbosa <dbarbosa@bu.edu>
2022-03-10 16:32:32 -0500

    added more serializers

--- mysite/myapi/serializer.py
+++ mysite/myapi/serializer.py
@@ -2,9 +2,20 @@
 
 from rest_framework import serializers
 
-from .models import User
+from .models import Device, MedHistory, User
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('firstName', 'lastName', 'id')
+
+class DeviceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Device
+        fields = ('deviceSKU', 'did', 'uid')
+
+class MedHistorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = MedHistory
+        fields = ('uid')
+    
--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -4,10 +4,18 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 
-from .serializer import UserSerializer
-from .models import User
+from .serializer import DeviceSerializer, MedHistorySerializer, UserSerializer
+from .models import Device, MedHistory, User
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('lastName')
     serializer_class = UserSerializer
+
+class DeviceViewSet(viewsets.ModelViewSet):
+    queryset = Device.objects.all().order_by('deviceSKU')
+    serializer_class = DeviceSerializer
+
+class MedHistoryViewSet(viewsets.ModelViewSet):
+    queryset = MedHistory.objects.all().order_by('uid')
+    serializer_class = MedHistorySerializer
